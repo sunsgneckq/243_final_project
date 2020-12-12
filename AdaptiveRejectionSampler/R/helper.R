35f254d6745423e74770f67f6bd7f259e0b8aa51
Colleen Sun <colleensun@Colleens-MacBook-Pro-2.local>
2020-12-12 14:37:30 -0800

    Finished helper.R documentation and added some tests

--- AdaptiveRejectionSampler/R/helper.R
+++ AdaptiveRejectionSampler/R/helper.R
@@ -1,103 +1,120 @@
-#' @description calculate the intercepts of the piecewise function u_j
-#' @param x evaluated x
-#' @param h h(x)
-#' @param dh derivative of h(x)
+#' @description Calculates where the tangents to \eqn{u_k(x)} at neighboring x
+#' values intersect. \eqn{u_k} is a piecewise linear upper hull formed by the 
+#' tangents to h(x). 
+#' @param x numeric vector of x values to evaluate the tangents at
+#' @param h concave function h(x) evaluated at x
+#' @param dh derivative of h(x) evaluated at x
 #' @param bounds pre-defined bounds for h(x)
-#' @output intercept of piecewise u_j(x)
+#' @return Numeric vector of where the tangents to \eqn{u_k} intersect, 
+#' including bounds values.
 
 intercept_z_j <- function(x, h, dh, bounds) {
   length_h <- length(h)
   return (append(bounds[1],
                  append((h[2:length_h] - h[1:length_h-1] -
-                           x[2:length_h]*dh[2:length_h] +
-                           x[1:length_h-1]*dh[1:length_h-1])/(dh[1:length_h-1] -
-                                                  dh[2:length_h]), bounds[2])))
+                           x[2:length_h] * dh[2:length_h] +
+                           x[1:length_h-1] * dh[1:length_h-1]) / 
+                          (dh[1:length_h-1] - dh[2:length_h]), bounds[2])))
 }
 
-#' @description calculate the slope and intercept of the  function u(x)
-#' @param x evaluated x
-#' @param h h(x)
-#' @param dh derivative of h(x)
-#' @output slope of piecewise u_j(x),
+
+#' @description Calculates the slopes and intercepts of piecewise function
+#' \eqn{u_k} at given x values.
+#' @param x numeric vector of x values to evaluate at
+#' @param h h(x) evaluated at x
+#' @param dh derivative of h(x) evaluated at x
+#' @return A list of length 2. \code{m} numeric vector of slopes, \code{b} 
+#' numeric vector of intercepts.
 
 slope_intercept_z_j <- function(x, h, dh) {
   return (list(m = dh, b = h - x*dh))
 }
 
-#' @description calculate the intercepts of the piecewise function l_j
-#' @param x evaluated x
-#' @param h h(x)
-#' @output intercept and slope of piecewise l_j(x)
+
+#' @description Calculates the slopes and intercepts of piecewise function 
+#' \eqn{l_j} at given x values. \eqn{l_j} is a piecewise linear lower hull 
+#' formed by adjacent chords.
+#' @param x numeric vector of x values to evaluate at
+#' @param h h(x) evaluated at x
+#' @return A list of length 2. \code{m} numeric vector of slopes, \code{b} 
+#' numeric vector of intercepts.
 
 slope_intercept_l_j <- function(x, h) {
-length_h <- length(h)
-return (list(m =  (h[2:length_h] - h[1:length_h-1])/(x[2:length_h] - x[1:length_h-1]),
-             b = (x[2:length_h]*h[1:length_h-1] -
-                    x[1:length_h-1]*h[2:length_h])/(x[2:length_h] -
-                                                      x[1:length_h-1])))
+  length_h <- length(h)
+  return (list(m = (h[2:length_h] - h[1:length_h-1]) / 
+                 (x[2:length_h] - x[1:length_h-1]),
+               b = (x[2:length_h] * h[1:length_h-1] - x[1:length_h-1] * 
+                      h[2:length_h]) / (x[2:length_h] - x[1:length_h-1])))
 }
 
 
-#' @description calculate the normalized beta and weights for sampling u(x)
-#' @param m slope
-#' @param b intercept
-#' @param bounds bounds for function
-#' @output normalized beta
-#' @output w: area
+#' @description Calculates the normalized beta and weights for sampling \eqn{u_k}.
+#' @param m numeric vector of slopes
+#' @param b numeric vector of intercepts
+#' @param bounds numeric vector of abscissae bounds for function
+#' @return A list of length 2. \code{beta} numeric vector of normalized beta, 
+#' \code{w} numeric vector of weights.
 
 beta_u_x <- function(m, b, bounds) {
-  ## Define the length of the bounds
+  # Number of abscissae bounds on the domain
   length_bounds <- length(bounds)
-  ## Assign the infinity and Nan to 0 for easy filter later
+  # Assign infinity and NaN to 0 for easy filter later
   exp_b_selection <- ifelse(is.finite(exp(b)), exp(b), 0.0)
-  ## Filter out slope m with none 0
+  # Filter out non-zero slope
   none_zero <- (m != 0.0)
-  ## Check all the selection of m do not contain 0 for the slope
+  # Check all the selection of m do not contain 0 for the slope
   assert_that(all(none_zero != 0.0))
+  # treat m = 0 case
+  sum_none_zero <- sum((exp_b_selection[none_zero] / m[none_zero]) * 
+                      (exp(m[none_zero] * bounds[2:length_bounds][none_zero]) -
+                      exp(m[none_zero] * bounds[1:length_bounds-1][none_zero])))
+  sum_z <- sum(exp_b_selection[!none_zero] * 
+                 (bounds[2:length_bounds][none_zero] - 
+                    bounds[1:length_bounds-1][!none_zero]))
 
-  ## treat m=0 case
-  summation_non_zero <- sum((exp_b_selection[none_zero]/m[none_zero])*(exp(m[none_zero]*bounds[2:length_bounds][none_zero]) - exp(m[none_zero]*bounds[1:length_bounds-1][none_zero])))
-  summation_z <- sum(exp_b_selection[!none_zero]*(bounds[2:length_bounds][none_zero] - bounds[1:length_bounds-1][!none_zero]))
   ## Check integration larger than 0
-  #assert_that(summation_z + summation_non_zero <= 0.0, msg = " s(x)=exp(u(x)) <= 0")
+  #assert_that(sum_z + sum_none_zero <= 0.0, msg = " s(x)=exp(u(x)) <= 0")
 
-  ## Normalize beta
-  beta_normalization <- exp_b_selection/(summation_non_zero + summation_z)
-  ## Calculate weights for all m, with separation for zero and none zero cases
-  weights <- ifelse(none_zero, (beta_normalization/m)*(exp(m*bounds[2:length_bounds]) - exp(m*bounds[1:length_bounds-1])), beta_normalization*(bounds[2:length_bounds] - bounds[1:length_bounds-1]))
-  return (list(beta = beta_normalization, w = ifelse((weights > 0) & is.finite(weights), weights, 0.0)))
+  # Normalize beta
+  beta_normalization <- exp_b_selection/(sum_none_zero + sum_z)
+  # Calculate weights for all m, with separation for zero and none zero cases
+  weights <- ifelse(none_zero, 
+                    (beta_normalization/m)*(exp(m*bounds[2:length_bounds]) - 
+                                              exp(m*bounds[1:length_bounds-1])), 
+                    beta_normalization*(bounds[2:length_bounds] - 
+                                          bounds[1:length_bounds-1]))
+  return(list(beta = beta_normalization, 
+              w = ifelse((weights > 0) & is.finite(weights), weights, 0.0)))
 }
 
-#' @description sample from u(x) function distribution, select segmment of sampling based on the areas and use inverse CDF for sampling
+
+#' @description Samples from \eqn{s_k(x)} function distribution, select segment 
+#' of sampling based on the areas and use inverse CDF for sampling
 #' @param N Number of samples
-#' @param beta normalized beta from above function
-#' @param m slope
-#' @param weights weights
-#' @param bounds bounds of intercepts
-#' @output sampled x and J
+#' @param beta normalized beta given by \code{beta_u_x} function
+#' @param m slope given by \code{slope_intercept_z_j} function
+#' @param weights weights given by \code{beta_u_x} function
+#' @param bounds intercepts given by \code{intercept_z_j} function
+#' @return A list of two vectors of length N of sampled x and J.
 
 sampling_x <- function(N, beta, m, weights, bounds) {
   sampling <- sample(length(weights), N, replace = TRUE, prob = weights)
   random_N <- runif(N)  # uniform random samples
-  ## Inverse CDF sampling of x
-  x_CDF <- ifelse(m[sampling] != 0, (1/m[sampling])*log((m[sampling]*weights[sampling]/beta[sampling])*random_N + exp(m[sampling]*bounds[sampling])), bounds[sampling] + weights[sampling]*random_N/beta[sampling])
+  # Inverse CDF sampling
+  x_CDF <- ifelse(m[sampling] != 0, 
+                  (1/m[sampling])*log((m[sampling]*weights[sampling]/beta[sampling])*random_N + 
+                                        exp(m[sampling]*bounds[sampling])), 
+                  bounds[sampling] + weights[sampling]*random_N/beta[sampling])
   return (list(x = x_CDF, J = sampling))
 }
 
 
+#' @description Checks whether the function h is log-concave.
+#' @param bound numeric 
+#' @param L numeric
+#' @param x numeric vector
+#' @return Gives an error message if the function h is not log-concave.
 
-max_iter_check <- function(i,max_iter){
-  if (i > max_iter) {
-    stop('You have reached the maximum number of iteration to prevent overflow')
-  }
-}
-
-boundary_warning <- function(boolean_check) {
-  if (!boolean_check) {
-    warning('Check bounds! The Sampled x is outside bounds ')
-    boolean_check <- TRUE
-  }
-}
 log_concavity_check <- function(bound, L, x) {
   if (L > 1) {
     ## Check log-concavity of function
@@ -106,6 +123,52 @@ log_concavity_check <- function(bound, L, x) {
     }
   }
 }
+
+
+#' @description Checks whether values are within given bounds.
+#' @param x numeric vector
+#' @param bounds numeric vector of length 2, lower bound and upper bound
+#' @return \code{TRUE} if \code{x} is within the given bounds, \code{FALSE} otherwise.
+boundary_check<- function(x, bound) {
+  ((x > bound[1]) & (x < bound[2]))
+}
+
+
+#' @description Whether the boundary condition is being checked.
+#' @param boolean_check boolean
+#' @return Gives a warning message if the \code{boolean_check} is \code{FALSE}.
+boundary_warning <- function(boolean_check) {
+  if (!boolean_check) {
+    warning('Check bounds! The sampled x is outside bounds')
+    boolean_check <- TRUE
+  }
+}
+
+
+#' @description Checks whether the algorithm has reached a maximum number of 
+#' iterations.
+#' @param i current iteration
+#' @param max_iter maximum number of iterations
+#' @return Gives an error message if the algorithm has reached the maximum number 
+#' of iterations.
+max_iter_check <- function(i,max_iter){
+  if (i > max_iter) {
+    stop('You have reached the maximum number of iteration to prevent overflow')
+  }
+}
+
+
+#' @description Checks for NaNs and infinities.
+#' @param x numeric vector
+#' @param y numeric vector
+#' @return \code{TRUE} if \code{x} and \code{y} are finite, \code{FALSE} otherwise.
+finite_check<- function(x,y) {
+  is.finite(x) && is.finite(y)
+}
+
+
+#' @description Breaks out of the current loop if the length of \code{x} is 1.
+#' @param x numeric vector
 length_stopper<- function(x){
   length_dh_k <- length(x)
   if (length_dh_k == 1) {
@@ -113,11 +176,6 @@ length_stopper<- function(x){
   }
 }
 
-finite_check<- function(x,y) {
-  is.finite(x) && is.finite(y)
-}
 
-boundary_check<- function(x, bound) {
-  ((x > bound[1]) & (x < bound[2]))
-}
+
 
--- AdaptiveRejectionSampler/tests/test-ars.R
+++ AdaptiveRejectionSampler/tests/test-ars.R
@@ -1,12 +1,19 @@
-#source("R/args.R")
 
 test_that("Missing arguments are dealt with", {
   expect_error(ars(), "Missing input arguments")
-  expect_error(ars(dnorm), "Missing input arguments")
   expect_error(ars(N=100), "Missing input arguments")
 })
 
-test_that("Input arguments are valid", {
-  expect_error(ars(10,10), "f must be a function")
-  expect_error(ars(dnorm,dnorm), "N must be numeric") #check is.numeric(N)
+test_that("Invalid inputs are dealt with", {
+  expect_error(ars(100, 100), "f must be a function")
+  expect_error(ars(dnorm, dnorm), "N must be a numeric input") 
+  expect_error(ars(dnorm, 10, bounds=2), "bounds must be length 2")
+  expect_error(ars(dnorm, 10, x0 = c(1,2,3)), "x0 must be length 2")
+  expect_error(ars(dnorm, 10, x0 = c(-1, 5), bounds = c(0, 0.1)), "x0 must be inside the bounds")
 })
+
+test_that("Inputting a log function", {
+  h <- function(x) log(dnorm(x))
+  expect_error(ars(h, 10), bounds = c(0,100))
+})
+
