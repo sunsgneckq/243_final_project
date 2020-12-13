3fdd56dc51c2d50483ed07abec2a4fcf2d389d98
Keqin Cao <caokeqin0211@gmail.com>
2020-12-13 12:47:53 -0800

    debugging done

--- AdaptiveRejectionSampler/R/args.R
+++ AdaptiveRejectionSampler/R/args.R
@@ -11,191 +11,195 @@
 library("assertthat")
 source("helper.R")
 
-ars <- function (f, N, x0 = c(-1.0 , 1.0),max_iter = 10000, bounds = c(-Inf, Inf), ...) {
-  ## Check if there is any missing input arguments
-  assertthat::assert_that(!missing(f), !missing(N), msg = "Missing input arguments")
-  ## Check if the input is a function
-  is_function = function(x) {
-    assert_that(is.function(x))
-  }
-  assert_that(is_function(f), msg = "f must be a function")
-  assert_that(is.numeric(N), msg = "N must be a numeric input")
-  ## Check that the bound has the length of 2
-  assert_that(length(bounds) == 2)
-  bound_length_2 <- function(x) {
-    length(x) == 2
-  }
-  ## Failure if the length of the bound is not equal to 2
-  on_failure(bound_length_2) = function(call, env) {
-    return("The length of the bounds (lower + upper) must be 2")
-  }
-  ## Check that the lower bound is not equal to upper bound
-  assert_that(bounds[1] != bounds[2])
-  ## Raise warning message and replace if they are the same
-  if (bounds[1] == bounds[2]) {
-    warning("Upper bound and lower bound cannot be the same, switch to default values")
-    bounds <- c(-Inf, Inf)
-  }
-  ## Check if lower bound is smaller than upper bound
-  assert_that(bounds[1] < bounds[2], msg = "Lower bound must be smaller than upper bound")
-  ## Check if lower bound is numeric input
-  assert_that(is.numeric(bounds[1]), msg = "lower bound must be numeric values")
-  ## Check if upper bound is numeric input
-  assert_that(is.numeric(bounds[2]), msg = "upper bound must be numeric values")
-  ## Check if x0 is within bounds
-  assert_that( all((x0 > bounds[1]) & (x0 < bounds[2])), msg = "x0 must be inside the bounds")
+ars <-
+  function (f,N,x0 = c(-1.0 , 1.0),bounds = c(-Inf, Inf),...) {
+    ## Check if there is any missing input arguments
+    assert_that(!missing(f),!missing(N), msg = "Missing input arguments")
+    ## Check if the input is a function
+    is_function = function(x) {
+      assert_that(is.function(x))
+    }
+    assert_that(is_function(f), msg = "f must be a function")
+    assert_that(is.numeric(N), msg = "N must be a numeric input")
+    ## Check that the bound has the length of 2
+    assert_that(length(bounds) == 2)
+    bound_length_2 <- function(x) {
+      length(x) == 2
+    }
+    ## Failure if the length of the bound is not equal to 2
+    on_failure(bound_length_2) = function(call, env) {
+      return("The length of the bounds (lower + upper) must be 2")
+    }
+    ## Check that the lower bound is not equal to upper bound
+    assert_that(bounds[1] != bounds[2])
+    ## Raise warning message and replace if they are the same
+    if (bounds[1] == bounds[2]) {
+      warning("Upper bound and lower bound cannot be the same, switch to default values")
+      bounds <- c(-Inf, Inf)
+    }
+    ## Check if lower bound is smaller than upper bound
+    assert_that(bounds[1] < bounds[2], msg = "Lower bound must be smaller than upper bound")
+    ## Check if lower bound is numeric input
+    assert_that(is.numeric(bounds[1]), msg = "lower bound must be numeric values")
+    ## Check if upper bound is numeric input
+    assert_that(is.numeric(bounds[2]), msg = "upper bound must be numeric values")
+    ## Check if x0 is within bounds
+    assert_that(all((x0 > bounds[1]) &
+                      (x0 < bounds[2])), msg = "x0 must be inside the bounds")
 
 
 
-  ## Define log function
-  h <- function(x) {
-    return (log(f(x, ...)))
-  }
+    ## Define log function
+    h <- function(x) {
+      return (log(f(x, ...)))
+    }
 
 
-  ## Define variables
-  dx <- 1E-8
-  x0<- sort(x0)
-  x_k <- c()
-  h_k <- c()
-  dh_k <- c()
-  x <- c()
-  h0 <- h(x0)
-  assert_that(length(h0) != 0, msg = "log function of x0 cannot be NaN/infinite")
+    ## Define variables
+    dx <- 1E-8
+    x0 <- sort(x0)
+    x_k <- c()
+    h_k <- c()
+    dh_k <- c()
+    x <- c()
+    h0 <- h(x0)
 
-  ## Finite difference approximation to h'(x)
-  dh0 <- (h(x0 + dx) - h0)/dx
-  ## Exclude invalid x0
-  x0 <- x0[finite_check(h0, dh0)]
-  ## Exclude invalid h0
-  h0 <- h0[finite_check(h0, dh0)]
-  ## Exclude invalid dh0
-  dh0 <- dh0[finite_check(h0, dh0)]
+    assert_that(length(h0) != 0, msg = "log function of x0 cannot be NaN/infinite")
 
+    ## Finite difference approximation to h'(x)
+    dh0 <- (h(x0 + dx) - h0) / dx
 
+    ## Exclude invalid x0
+    x0 <- x0[finite_check(h0, dh0)]
+    ## Exclude invalid h0
+    h0 <- h0[finite_check(h0, dh0)]
+    ## Exclude invalid dh0
+    dh0 <- dh0[finite_check(h0, dh0)]
 
-  ##### Main Function
-  i <- 0
-  dx <- 1E-8
-  check_boundary <- FALSE
-  ## Loop until we have N samples (or until error)
-  while (length(x) < N) {
-    i <- i + 1
-    max_iter_check(i, max_iter)
-    chunk_size_vectorized <- min(c(N, i**2))
+    ##### Main Function
+    i <- 0
+    dx <- 1E-8
+    max_iter <- 15000
+    check_boundary <- FALSE
+    ## Loop until we have N samples
+    while (length(x) < N) {
+      i <- i + 1
+      max_iter_check(i, max_iter)
+      chunk_size_vectorized <- min(c(N, i ** 2))
     ##### INITIALIZATION
 
-    ## only re-initialize if there are new samples
-    if (0 < length(x0) ) {
+      if (0 < length(x0)) {
+        ## Update
+        x_k <- append(x_k, x0)
+        h_k <- append(h_k, h0)
+        dh_k <- append(dh_k, dh0)
+        x0 <- c()
+        h0 <- c()
+        dh0 <- c()
+        ## Sort x_k's in ascending order
+        sorted_string_index <- sort(x_k, index.return = TRUE)
+        x_k <- sorted_string_index$x
+        h_k <- h_k[sorted_string_index$ix]
+        dh_k <- dh_k[sorted_string_index$ix]
 
-      ## Update
-      x_k <- append(x_k, x0)
-      h_k <- append(h_k, h0)
-      dh_k <- append(dh_k, dh0)
-      x0 <- c()
-      h0 <- c()
-      dh0 <- c()
-      ## Sort x_k's in ascending order
-      sorted_string_index <- sort(x_k, index.return = TRUE)
-      x_k <- sorted_string_index$x
-      h_k <- h_k[sorted_string_index$ix]
-      dh_k <- dh_k[sorted_string_index$ix]
+        length_dh_k <- length(dh_k)
 
-      length_dh_k <- length(dh_k)
 
-      ## Check for duplicates in x and h'(x)
-      if (length_dh_k > 1) {
-        while (!all((abs(dh_k[1:length_dh_k-1] - dh_k[2:length_dh_k]) > 1E-8)
-                    & ((x_k[2:length_dh_k] - x_k[1:length_dh_k-1]) > dx))) {
+        dx <-1E-8  # mesh size for finite difference approximation to h'(x)
+        eps <- 1E-7
+        if (length_dh_k > 1) {
+          while (!all((abs(dh_k[1:length_dh_k - 1] - dh_k[2:length_dh_k]) > eps) &
+                      ((x_k[2:length_dh_k] - x_k[1:length_dh_k - 1]) > dx))) {
+            ## Check for duplication and remove
+            x_k <- x_k[duplication_check(dh_k, x_k, length_dh_k, eps, dx)]
+            h_k <- h_k[duplication_check(dh_k, x_k, length_dh_k, eps, dx)]
+            dh_k <- dh_k[duplication_check(dh_k, x_k, length_dh_k, eps, dx)]
 
-          ## Only keep values does not have same adjacent values
-          duplication_neg <- append(TRUE, ((abs(dh_k[1:length_dh_k-1] - dh_k[2:length_dh_k]) > 1E-8) &
-                                             ((x_k[2:length_dh_k] - x_k[1:length_dh_k-1]) > dx)))
-          x_k <- x_k[duplication_neg]
-          h_k <- h_k[duplication_neg]
-          dh_k <- dh_k[duplication_neg]
-
-          length_stopper(dh_k)
+            length_dh_k <- length(dh_k)
+            if (length_dh_k == 1) {
+              break
+            }
+          }
+          #log concavity check
+          log_concavity_check(eps, length_dh_k, dh_k)
         }
-        log_concavity_boundary <- 1E-8
-        log_concavity_check(log_concavity_boundary,length_dh_k,dh_k)
+
+
+
+
+        ## Use helper function to compute z_k, u_k(x), l_k(x), s_k(x)
+        z_k <- intercept_z_j(x_k, h_k, dh_k, bounds)
+        u_k <- slope_intercept_z_j(x_k, h_k, dh_k)
+        l_k <- slope_intercept_l_j(x_k, h_k)
+        s_k <- beta_u_x(u_k$m, u_k$b, z_k)
       }
 
+      ##### SAMPLING STEP
+
+      ## draw x from exp(u(x))
+      sampling_x <-
+        sampling_x(chunk_size_vectorized, s_k$beta, u_k$m, s_k$w, z_k)
+      x_s <- sampling_x$x
+
+      J <- sampling_x$J
+
+      ## random uniform for rejection sampling
+      w <- runif(chunk_size_vectorized)
+
+      ## Warn if samples were outside of bounds
+      ## This happens if bounds given don't reflect
+      ## the actual bounds of the distribution
+      if (!all(boundary_check(x_s, bounds))) {
+        ## Flag so warning only happens once
+        boundary_warning(check_boundary)
+        ## Filter out values that are outside bounds
+        J <- J[boundary_check(x_s, bounds)]
+        x_s <- x_s[boundary_check(x_s, bounds)]
+        w <- w[boundary_check(x_s, bounds)]
+      }
+
+      ## only use x-values where l_k(x) > -Inf
+      boundary <- (J - ifelse(x_s < x_k[J], 1, 0) >= 1) &
+        (J - ifelse(x_s < x_k[J], 1, 0) < length(x_k))
+
+      ## Perform first rejection test on u(x)/length_dh_k(x)
+      # New Index for J
+      new_index <- J - ifelse(x_s < x_k[J], 1, 0)
+      y <- exp(x_s[boundary] * (l_k$m[new_index[boundary]] -
+                                  u_k$m[J[boundary]]) +
+                 l_k$b[new_index[boundary]] -
+                 u_k$b[J[boundary]])
+      first_rejection_accept <- (w[boundary] <= y)
+
+      ## Append accepted values to x
+      x <- append(x, x_s[boundary][first_rejection_accept])
+      ## Append rejected values to x0
+      x0 <- append(x_s[!boundary],
+                   x_s[boundary][!first_rejection_accept])
+
+      if (length(x0) > 0) {
+        ## Evaluate h(x0)
+        h0 <- h(x0)
+        ## Finite difference approximation to h'(x)
+        dh0 <- (h(x0 + dx) - h0) / dx
+
+        ## Check for NaNs and infinities
+        x0 <- x0[finite_check(h0, dh0)]
+        h0 <- h0[finite_check(h0, dh0)]
+        dh0 <- dh0[finite_check(h0, dh0)]
 
 
-      ## Use helper function to compute z_k, u_k(x), l_k(x), s_k(x)
-      z_k <- intercept_z_j(x_k, h_k, dh_k, bounds)
-      u_k <- slope_intercept_z_j(x_k, h_k, dh_k)
-      l_k <- slope_intercept_l_j(x_k, h_k)
-      s_k <- beta_u_x(u_k$m, u_k$b, z_k)
+        w <- append(w[!boundary], w[boundary]
+                    [!first_rejection_accept])[finite_check(h0, dh0)]
+        J <- append(J[!boundary], J[boundary]
+                    [!first_rejection_accept])[finite_check(h0, dh0)]
+
+        ## Append accepted values of the second rejection test on the u(x)/h(x)
+        x <- append(x, x0[(w <= exp(h0 - x0 * u_k$m[J] - u_k$b[J]))])
+      }
     }
+    # Return all N samples in x
+    res <- x[1:N]
 
-    ##### SAMPLING STEP
-
-    ## draw x from exp(u(x))
-    sampling_x <- sampling_x(chunk_size_vectorized, s_k$beta, u_k$m, s_k$w, z_k)
-    x_s <- sampling_x$x
-    J <- sampling_x$J
-
-    ## random uniform for rejection sampling
-    w <- runif(chunk_size_vectorized)
-
-    ## Warn if samples were outside of bounds
-    ## This happens if bounds given don't reflect
-    ## the actual bounds of the distribution
-    if (!all(boundary_check(x_s, bounds))) {
-
-      ## Flag so warning only happens once
-      boundary_warning(check_boundary)
-      ## Filter out values that are outside bounds
-      J <- J[boundary_check(x_s, bounds)]
-      x_s <- x_s[boundary_check(x_s, bounds)]
-      w <- w[boundary_check(x_s, bounds)]
-    }
-
-    ## Index shift for l_k(x)
-
-    ## only use x-values where l_k(x) > -Inf
-    boundary <- (J - ifelse(x_s < x_k[J], 1, 0) >= 1) &
-      (J - ifelse(x_s < x_k[J], 1, 0) < length(x_k))
-
-    ## Perform first rejection test on u(x)/length_dh_k(x)
-    # New Index for J
-    new_index <- J - ifelse(x_s < x_k[J], 1, 0)
-    y <- exp(x_s[boundary]*(l_k$m[new_index[boundary]] -
-                              u_k$m[J[boundary]]) +
-               l_k$b[new_index[boundary]] -
-               u_k$b[J[boundary]])
-    first_rejection_accept <- (w[boundary] <= y)
-
-    ## Append accepted values to x
-    x <- append(x, x_s[boundary][first_rejection_accept])
-    ## Append rejected values to x0
-    x0 <- append(x_s[!boundary],
-                 x_s[boundary][!first_rejection_accept])
-
-    if (length(x0) > 0) {
-      ## Evaluate h(x0)
-      h0 <- h(x0)
-      ## Finite difference approximation to h'(x)
-      dh0 <- (h(x0 + dx) - h0)/dx
-
-      ## Check for NaNs and infinities
-      x0 <- x0[finite_check(h0, dh0)]
-      h0 <- h0[finite_check(h0, dh0)]
-      dh0 <- dh0[finite_check(h0, dh0)]
-
-
-      w <- append(w[!boundary], w[boundary]
-                  [!first_rejection_accept])[finite_check(h0, dh0)]
-      J <- append(J[!boundary], J[boundary]
-                  [!first_rejection_accept])[finite_check(h0, dh0)]
-
-      ## Append accepted values of the second rejection test on the u(x)/h(x)
-      x <- append(x, x0[ (w <= exp(h0 - x0*u_k$m[J] - u_k$b[J]))])
-    }
+    return(res)
   }
-
-  ## Return N samples
-  return (x[1:N])
-}
--- AdaptiveRejectionSampler/R/helper.R
+++ AdaptiveRejectionSampler/R/helper.R
@@ -1,3 +1,4 @@
+
 #' @description calculate the intercepts of the piecewise function u_j
 #' @param x evaluated x
 #' @param h h(x)
@@ -8,64 +9,81 @@
 intercept_z_j <- function(x, h, dh, bounds) {
   length_h <- length(h)
   return (append(bounds[1],
-                 append((h[2:length_h] - h[1:length_h-1] -
-                           x[2:length_h]*dh[2:length_h] +
-                           x[1:length_h-1]*dh[1:length_h-1])/(dh[1:length_h-1] -
-                                                  dh[2:length_h]), bounds[2])))
+                 append((h[2:length_h] - h[1:length_h - 1] -
+                           x[2:length_h] * dh[2:length_h] +
+                           x[1:length_h - 1] * dh[1:length_h - 1]) / (dh[1:length_h -
+                                                                           1] -
+                                                                        dh[2:length_h]), bounds[2]
+                 )))
 }
 
-#' @description calculate the slope and intercept of the  function u(x)
+#' @description calculate the slope and intercept of the function u(x)
 #' @param x evaluated x
 #' @param h h(x)
 #' @param dh derivative of h(x)
-#' @output slope of piecewise u_j(x),
+#' @output slope of piece-wise u_j(x),
 
 slope_intercept_z_j <- function(x, h, dh) {
-  return (list(m = dh, b = h - x*dh))
+  return (list(m = dh, b = h - x * dh))
 }
 
-#' @description calculate the intercepts of the piecewise function l_j
+#' @description calculate the intercepts of the piece-wise function l_j
 #' @param x evaluated x
 #' @param h h(x)
-#' @output intercept and slope of piecewise l_j(x)
+#' @output intercept and slope of piece-wise l_j(x)
 
 slope_intercept_l_j <- function(x, h) {
-length_h <- length(h)
-return (list(m =  (h[2:length_h] - h[1:length_h-1])/(x[2:length_h] - x[1:length_h-1]),
-             b = (x[2:length_h]*h[1:length_h-1] -
-                    x[1:length_h-1]*h[2:length_h])/(x[2:length_h] -
-                                                      x[1:length_h-1])))
+  length_h <- length(h)
+  return (list(
+    m =  (h[2:length_h] - h[1:length_h - 1]) /
+      (x[2:length_h] - x[1:length_h -1]),
+    b = (x[2:length_h] * h[1:length_h - 1] -
+           x[1:length_h - 1] * h[2:length_h]) / (x[2:length_h] -
+                                                   x[1:length_h - 1])
+  ))
 }
 
+
+integration_check <- function(x, y) {
+  if (x + y <= 0.0) {
+    stop('Area of s(x)=exp(u(x)) <= 0')
+  }
+}
 
 #' @description calculate the normalized beta and weights for sampling u(x)
 #' @param m slope
 #' @param b intercept
 #' @param bounds bounds for function
 #' @output normalized beta
-#' @output w: area
+#' @output w: area weights
 
-beta_u_x <- function(m, b, bounds) {
-  ## Define the length of the bounds
+beta_u_x <- function (m, b, bounds) {
   length_bounds <- length(bounds)
-  ## Assign the infinity and Nan to 0 for easy filter later
-  exp_b_selection <- ifelse(is.finite(exp(b)), exp(b), 0.0)
-  ## Filter out slope m with none 0
-  none_zero <- (m != 0.0)
-  ## Check all the selection of m do not contain 0 for the slope
-  assert_that(all(none_zero != 0.0))
+  b_selection_finite <- ifelse(is.finite(exp(b)), exp(b), 0.0)
 
   ## treat m=0 case
-  summation_non_zero <- sum((exp_b_selection[none_zero]/m[none_zero])*(exp(m[none_zero]*bounds[2:length_bounds][none_zero]) - exp(m[none_zero]*bounds[1:length_bounds-1][none_zero])))
-  summation_z <- sum(exp_b_selection[!none_zero]*(bounds[2:length_bounds][none_zero] - bounds[1:length_bounds-1][!none_zero]))
-  ## Check integration larger than 0
-  #assert_that(summation_z + summation_non_zero <= 0.0, msg = " s(x)=exp(u(x)) <= 0")
+  non_zero_boolean <- (m != 0.0)
+  non_zero_boolean_summation <- sum((b_selection_finite[non_zero_boolean]
+                               / m[non_zero_boolean]) *
+                                (exp(m[non_zero_boolean] *
+                                       bounds[2:length_bounds][non_zero_boolean]) -
+                                   exp(m[non_zero_boolean] *
+                                         bounds[1:length_bounds -
+                                                  1][non_zero_boolean])))
+  summation_z <- sum(b_selection_finite[!non_zero_boolean] *
+                 (bounds[2:length_bounds][!non_zero_boolean] -
+                    bounds[1:length_bounds - 1][!non_zero_boolean]))
 
+  ## Enforce that the integral of s(x) > 0
+  integration_check(non_zero_boolean_summation, summation_z)
   ## Normalize beta
-  beta_normalization <- exp_b_selection/(summation_non_zero + summation_z)
-  ## Calculate weights for all m, with separation for zero and none zero cases
-  weights <- ifelse(none_zero, (beta_normalization/m)*(exp(m*bounds[2:length_bounds]) - exp(m*bounds[1:length_bounds-1])), beta_normalization*(bounds[2:length_bounds] - bounds[1:length_bounds-1]))
-  return (list(beta = beta_normalization, w = ifelse((weights > 0) & is.finite(weights), weights, 0.0)))
+  beta <- b_selection_finite / (non_zero_boolean_summation + summation_z)
+  ## Calculate weights for both m!=0 and m=0 cases
+  weight <-
+    ifelse(non_zero_boolean, (beta / m) * (exp(m * bounds[2:length_bounds]) -
+                                             exp(m * bounds[1:length_bounds - 1])),
+           beta * (bounds[2:length_bounds] - z[1:length_bounds -1]))
+  return (list(beta = beta, w = ifelse((weight > 0) & is.finite(weight), weight, 0.0)))
 }
 
 #' @description sample from u(x) function distribution, select segmment of sampling based on the areas and use inverse CDF for sampling
@@ -77,16 +95,21 @@ beta_u_x <- function(m, b, bounds) {
 #' @output sampled x and J
 
 sampling_x <- function(N, beta, m, weights, bounds) {
-  sampling <- sample(length(weights), N, replace = TRUE, prob = weights)
+  sampling <-
+    sample(length(weights), N, replace = TRUE, prob = weights)
   random_N <- runif(N)  # uniform random samples
   ## Inverse CDF sampling of x
-  x_CDF <- ifelse(m[sampling] != 0, (1/m[sampling])*log((m[sampling]*weights[sampling]/beta[sampling])*random_N + exp(m[sampling]*bounds[sampling])), bounds[sampling] + weights[sampling]*random_N/beta[sampling])
+  x_CDF <-
+    ifelse(m[sampling] != 0,
+           (1 / m[sampling]) * log((m[sampling] * weights[sampling] / beta[sampling]) *
+                                     random_N + exp(m[sampling] * bounds[sampling])),
+           bounds[sampling] + weights[sampling] * random_N / beta[sampling])
   return (list(x = x_CDF, J = sampling))
 }
 
 
 
-max_iter_check <- function(i,max_iter){
+max_iter_check <- function(i, max_iter) {
   if (i > max_iter) {
     stop('You have reached the maximum number of iteration to prevent overflow')
   }
@@ -98,26 +121,27 @@ boundary_warning <- function(boolean_check) {
     boolean_check <- TRUE
   }
 }
+
 log_concavity_check <- function(bound, L, x) {
   if (L > 1) {
     ## Check log-concavity of function
-    if(!all((x[1:L-1] - x[2:L]) >= bound)) {
+    if (!all((x[1:L - 1] - x[2:L]) >= bound)) {
       stop('Input function f not log-concave.')
     }
   }
 }
-length_stopper<- function(x){
-  length_dh_k <- length(x)
-  if (length_dh_k == 1) {
-    break
-  }
+
+
+finite_check <- function(x, y) {
+  is.finite(x) & is.finite(y)
 }
 
-finite_check<- function(x,y) {
-  is.finite(x) && is.finite(y)
-}
-
-boundary_check<- function(x, bound) {
+boundary_check <- function(x, bound) {
   ((x > bound[1]) & (x < bound[2]))
 }
 
+duplication_check <- function(x, y, L, eps, dx) {
+  dup <-
+    append(TRUE, ((abs(x[1:L - 1] - x[2:L]) > eps) &
+                    ((y[2:L] - y[1:L - 1]) > dx)))
+}
