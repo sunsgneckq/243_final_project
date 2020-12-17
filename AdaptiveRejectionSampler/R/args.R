5ccfce316ef8c8c876166f8862b39a0e808497be
Keqin Cao <caokeqin0211@gmail.com>
2020-12-16 19:29:23 -0800

    annotation

--- AdaptiveRejectionSampler/R/args.R
+++ AdaptiveRejectionSampler/R/args.R
@@ -3,12 +3,13 @@
 ## Adaptive Rejection Sampling Algorithm
 ##
 ## Date: 12/15/2020
-## Authors: Keqin Cao
+## Authors: Keqin Cao, Mark Campmier, Colleen Sun
 ###########################################################
 
 
 # Load Required Packages
 library("assertthat")
+# Source the helper functions
 source("helper.R")
 
 ars <-
@@ -19,8 +20,14 @@ ars <-
     is_function = function(x) {
       assert_that(is.function(x))
     }
+    ## Check if the input f is a function
     assert_that(is_function(f), msg = "f must be a function")
-    assert_that(is.numeric(N), msg = "N must be a numeric input")
+    ## Check if the N sample size is integer
+    assert_that(is.integer(N), msg = "N must be a numeric integer input")
+    ## Check if the N sample size is larger than 0
+    assert_that(N > 0, msg = "N must be larger than 0")
+    ## Check if the x0 has all numeric input
+    assert_that(is.numeric(x0), msg = "x0 must be numeric input")
     ## Check that the bound has the length of 2
     assert_that(length(bounds) == 2, msg = "length of bounds must be 2")
     ## Raise warning message and replace if they are the same
@@ -46,17 +53,20 @@ ars <-
     }
 
 
-    ## Define variables
+    ## Sort x0 in order
     x0 <- sort(x0)
+    ## Define and init some variables
     x_k <- c()
     h_k <- c()
     dh_k <- c()
     x <- c()
-
+    ## Take the log function of x0 as defined
     h0 <- h(x0)
+    ## Check if length of h0 is 0
     assert_that(length(h0) != 0, msg = "log function of x0 cannot be NaN/infinite")
-    ## Finite difference approximation to h'(x)
+    ## Define a finite dx value
     dx <- 1E-8
+    ## Cauculate dh0 finite difference
     dh0 <- (h(x0 + dx) - h0) / dx
 
     ## Exclude invalid x0
@@ -67,15 +77,18 @@ ars <-
     dh0 <- dh0[finite_check(h0, dh0)]
 
     #### Main Function ####
+    ## Set the starter i to be 0
     i <- 0
+    ## Set the maximum iteration before stops to be 15000
     max_iter <- 15000
+    ## Define the boolean
     check_boundary <- FALSE
     ## Loop until we have N samples
     while (length(x) < N) {
       i <- i + 1
       max_iter_check(i, max_iter)
       chunk_size_vectorized <- min(c(N, i ** 2))
-    #### INITIALIZATION ####
+    #### Init ####
     ## Length of x0 cannot be 0
       if (0 < length(x0)) {
         ## Update
@@ -90,26 +103,29 @@ ars <-
         dh0 <- c()
         ## Sort x_k's in ascending order
         sorted_string_index <- sort(x_k, index.return = TRUE)
+        ## Filter sorted x_k value
         x_k <- sorted_string_index$x
+        ## Filter sorted h_k value
         h_k <- h_k[sorted_string_index$ix]
+        ## Filter sorted dh_k value
         dh_k <- dh_k[sorted_string_index$ix]
-
+        ## Compute the length of dh_k
         length_dh_k <- length(dh_k)
+        ## Define the boundary
         eps <- 1E-7
         if (length_dh_k > 1) {
           while (!all((abs(dh_k[1:length_dh_k - 1] - dh_k[2:length_dh_k]) > eps) &
                       ((x_k[2:length_dh_k] - x_k[1:length_dh_k - 1]) > dx))) {
-            ## Check for duplication and remove
+            ## Check for duplication and remove based on condition
             x_k <- x_k[duplication_check(dh_k, x_k, length_dh_k, eps, dx)]
             h_k <- h_k[duplication_check(dh_k, x_k, length_dh_k, eps, dx)]
             dh_k <- dh_k[duplication_check(dh_k, x_k, length_dh_k, eps, dx)]
-
             length_dh_k <- length(dh_k)
             if (length_dh_k == 1) {
               break
             }
           }
-          #log concavity check
+          ## Log concavity check
           log_concavity_check(eps, length_dh_k, dh_k)
         }
 
@@ -127,18 +143,16 @@ ars <-
 
       ## draw x from exp(u(x))
       sampling_x <-
-        sampling_x(chunk_size_vectorized, s_k$beta, u_k$m, s_k$w, z_k)
+      sampling_x(chunk_size_vectorized, s_k$beta, u_k$m, s_k$w, z_k)
       x_s <- sampling_x$x
       J <- sampling_x$J
 
       ## random uniform for rejection sampling
       w <- runif(chunk_size_vectorized)
 
-      ## Warn if samples were outside of bounds
-      ## This happens if bounds given don't reflect
-      ## the actual bounds of the distribution
+      ## Check how close it is to the distribution
       if (!all(boundary_check(x_s, bounds))) {
-        ## Flag so warning only happens once
+        ## Check if the samples are inside bounds
         boundary_warning(check_boundary)
         ## Filter out values that are outside bounds
         J <- J[boundary_check(x_s, bounds)]
@@ -150,9 +164,9 @@ ars <-
       boundary <- (J - ifelse(x_s < x_k[J], 1, 0) >= 1) &
         (J - ifelse(x_s < x_k[J], 1, 0) < length(x_k))
 
-      ## Perform first rejection test on u(x)/length_dh_k(x)
       # New Index for J
       new_index <- J - ifelse(x_s < x_k[J], 1, 0)
+      ## Perform first rejection test on u(x)/length_dh_k(x)
       y <- exp(x_s[boundary] * (l_k$m[new_index[boundary]] -
                                   u_k$m[J[boundary]]) +
                  l_k$b[new_index[boundary]] -
@@ -175,19 +189,16 @@ ars <-
         x0 <- x0[finite_check(h0, dh0)]
         h0 <- h0[finite_check(h0, dh0)]
         dh0 <- dh0[finite_check(h0, dh0)]
-
-
+        ## Append first rejection accept value with finite check ready
         w <- append(w[!boundary], w[boundary]
                     [!first_rejection_accept])[finite_check(h0, dh0)]
         J <- append(J[!boundary], J[boundary]
                     [!first_rejection_accept])[finite_check(h0, dh0)]
-
         ## Append accepted values of the second rejection test on the u(x)/h(x)
         x <- append(x, x0[(w <= exp(h0 - x0 * u_k$m[J] - u_k$b[J]))])
       }
     }
     # Return all N samples in x
     res <- x[1:N]
-
     return(res)
   }
